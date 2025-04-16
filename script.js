94de8ab759e3bb561fce2d9f0e912c253165c6a8
Arda Inegol <ardainegol10@gmail.com>
2025-04-16 13:03:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   if (toggle && nav) {
     toggle.addEventListener("click", function () {
       nav.classList.toggle("nav-active");
+      toggle.classList.toggle("open");
     });
 
     const navLinks = nav.querySelectorAll("a");
@@ -12,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
       link.addEventListener("click", () => {
         if (window.innerWidth <= 768) {
           nav.classList.remove("nav-active");
+          toggle.classList.remove("open");
         }
       });
     });
